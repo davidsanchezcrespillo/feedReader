baba0dfdbb90ece86417ff797037b47034747b38
David Sanchez Crespillo <davidsanchezcrespillo@gmail.com>
2014-06-28 14:50:13 +0200

    Separation of data into another file

---
+++ data.js
@@ -0,0 +1,51 @@
+
+/**
+ * Feed sources.
+ */
+var allSources = [
+  { 
+    name: 'LRT', 
+    url: 'http://gdata.youtube.com/feeds/base/users/LRTinklas/uploads?alt=rss&amp;v=2&amp;orderby=published&amp;client=ytapi-youtube-profile',
+    tags: ['media', 'tv', 'news']
+  },
+  {
+	name: 'Lithuanian Out Loud',
+	url: 'http://lithuanian.libsyn.com/rss',
+	tags: ['learning']  
+  },
+  {
+	name: 'Ziniu Radijas',
+	url: 'http://gdata.youtube.com/feeds/base/users/ZiniuTV/uploads?alt=rss&amp;v=2&amp;orderby=published&amp;client=ytapi-youtube-profile',
+	tags: ['media', 'radio', 'news']
+  },
+  {
+	name: 'Lietuvos Rytas',
+	url: 'http://www.lrytas.lt/rss/',
+	tags: ['media', 'news']
+  },
+  {
+	  name: 'Lietuviskos Zinios',
+	  url: 'http://lzinios.lt/rss/all/',
+	  tags: ['news']
+  },
+  {
+	  name: 'Bernardinai',
+	  url: 'http://www.bernardinai.lt/rss/news',
+	  tags: ['media', 'news']
+  }
+];
+
+/**
+ * Feed tags.
+ */
+var allTags = [
+  {name: 'media'},
+  {name: 'tv'},
+  {name: 'radio'},
+  {name: 'news'},
+  {name: 'learning'}
+];
+
+var appTitle = 'LT Feeds';
+
+
